ecf183d2dc8d0fe4c26c51b41200c470e63cfe4c
juablaz <jc-blazquez@hotmail.com>
2021-02-02 17:47:07 +0100

    Correcciones CDCT

--- CDCT/toposervice/src/test/java/es/codeurjc/mastercloudapps/topo/contracts/BaseCDC.java
+++ CDCT/toposervice/src/test/java/es/codeurjc/mastercloudapps/topo/contracts/BaseCDC.java
@@ -1,10 +1,14 @@
 package es.codeurjc.mastercloudapps.topo.contracts;
 
 import es.codeurjc.mastercloudapps.topo.controller.TopoController;
+import es.codeurjc.mastercloudapps.topo.model.City;
+import es.codeurjc.mastercloudapps.topo.service.TopoService;
 import io.restassured.module.mockmvc.RestAssuredMockMvc;
 import org.junit.jupiter.api.BeforeEach;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
 public abstract class BaseCDC {
@@ -12,8 +16,15 @@ public abstract class BaseCDC {
     @Autowired
     TopoController topoController;
 
+    @MockBean
+    TopoService topoService;
+
+    // Es necesario mockear el servicio, no podemos basarnos en los datos de inicialización
     @BeforeEach
     public void setup() {
+        Mockito.when(this.topoService.getCity(Mockito.anyString()))
+            .thenReturn(new City("Madrid", "Flat"));
+
         RestAssuredMockMvc.standaloneSetup(topoController);
     }
 }
